22bd338368b941591e99488bd0bf54a0cc1c0760
Mariangelica Rodriguez <gromarant@gmail.com>
2023-04-14 12:14:12 +0200

    Solve Opposites Attract- codewars exercise

---
+++ codeWars/oppositesAtract.js
@@ -0,0 +1,7 @@
+// first approach
+const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2;
+
+//Second approach
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
