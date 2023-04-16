42fcb8e32c73c8e0c64e1f014ee8190d89b116da
Mariangelica Rodriguez <gromarant@gmail.com>
2023-04-16 18:52:44 +0200

    Solve You Can't Code Under Pressure #1

---
+++ codewars/0_basicos_strings-y-numeros/1-youCantCodeUnderPressure1.js
@@ -0,0 +1,10 @@
+//First approach
+const doubleInteger = (i) => i * 2;
+
+//Second approach
+const doubleInteger = (i) => i.toFixed(0) * 2;
+
+//Third approach
+function doubleInteger(i) {
+  return i*2;
+}
