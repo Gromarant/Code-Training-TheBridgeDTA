a38040238022fad2bd1eff6d3cc33298df3b079c
Mariangelica Rodriguez <gromarant@gmail.com>
2023-04-16 21:29:52 +0200

    restore version

--- codeWars/oppositesAtract.js
+++
@@ -1,7 +0,0 @@
-// first approach
-const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2;
-
-//Second approach
-function lovefunc2(flower1, flower2) {
-  return flower1 % 2 !== flower2 % 2;
-}
--- codeWars/youCantUnderPressure1.js
+++
@@ -1,7 +0,0 @@
-//first approach
-const doubleInteger = (i) => i * 2;
-
-//Second approach
-function doubleInteger2(i) {
-  return i * 2;
-};
